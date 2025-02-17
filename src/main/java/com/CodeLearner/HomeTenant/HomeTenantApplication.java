1a8b40535554d0398f83af53896502305ead638a
ndihoeric <endihokubwayo@asystresources.com>
2025-02-17 11:31:01 +0200

    first new commit

---
+++ src/main/java/com/CodeLearner/HomeTenant/HomeTenantApplication.java
@@ -0,0 +1,13 @@
+package com.CodeLearner.HomeTenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTenantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodeLearner/HomeTenant/HomeTenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeLearner.HomeTenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
